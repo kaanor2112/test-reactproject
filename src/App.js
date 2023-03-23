df976fe1e73446e4b4ff9b64abc8d8444c320dc5
kaanor <kaanor@Linux.myguest.virtualbox.org>
2023-03-23 17:26:46 -0400

    initializing project appbot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
+import Button from './components/Button';
+import Input from './components/Input';
+import Logo from './components/Logo';
+
 function App() {
+  const [form1, setform1] = useState({ email: '', name: '', number: '', home: '', referral: '' })
+
+  const handleform1 = () => {
+    console.log(form1);
+    setform1({ "email": "", "home": "", "name": "", "number": "", "referral": "" });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo />
+        <h3>Join our waiting list</h3>
       </header>
+      <div className='App-container'>
+        <Input
+          name="name"
+          placeholder="Full Name"
+          type="Full Name"
+          value={form1.name}
+          onChange={e => setform1(v => ({ ...v, name: e.target.value }))}
+        />
+        <Input
+          name="email"
+          placeholder="Email address"
+          type="Email address"
+          value={form1.email}
+          onChange={e => setform1(v => ({ ...v, email: e.target.value }))}
+        />
+        <Input
+          name="number"
+          placeholder="Phone Number"
+          type="Phone Number"
+          value={form1.number}
+          onChange={e => setform1(v => ({ ...v, number: e.target.value }))}
+        />
+        <Input
+          name="home"
+          placeholder="Home Address"
+          type="Home Address"
+          value={form1.home}
+          onChange={e => setform1(v => ({ ...v, home: e.target.value }))}
+        />
+        <Input
+          name="referral"
+          placeholder="Referral"
+          type="Referral"
+          value={form1.referral}
+          onChange={e => setform1(v => ({ ...v, referral: e.target.value }))}
+        />
+        <Button onClick={handleform1}>Submit</Button>
+      </div>
     </div>
   );
 }
